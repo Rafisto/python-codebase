1178f4e6ac54d54278ff61be8fe8f0463d734084
Rafistarch <rvrelay@gmail.com>
2022-02-17 09:51:24 +0100

    add-1

--- factorialprimedecomposition.py
+++ factorialprimedecomposition.py
@@ -2,28 +2,8 @@
 
 import math
 
-
 def decomp(d):
-	factors = []
-	for n in range(2, d + 1):
-		x = n
-		while x % 2 == 0:
-			factors.append(2)
-			x = x / 2
-		for i in range(3, int(math.sqrt(x)) + 1, 2):
-			while x % i == 0:
-				factors.append(i)
-				x = x / i
-		if x > 2:
-			factors.append(int(x))
-	value = ''
-	for x in range(2, d + 1):
-		if factors.count(x) != 0:
-			if factors.count(x) == 1:
-				value += str(x) + ' * '
-			else:
-				value += str(x) + '^' + str(factors.count(x)) + ' * '
-	return value[:-3]
+
 
 
 print(decomp(14))
